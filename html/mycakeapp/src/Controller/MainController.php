ab64d8dd6adb757abe44b18c8687e3a94aef2a30
Satu <kttth0320@gmail.com>
2021-02-25 05:45:07 +0900

    料金割引一覧のアクション作成・モデル読み込み

--- html/mycakeapp/src/Controller/MainController.php
+++ html/mycakeapp/src/Controller/MainController.php
@@ -9,9 +9,19 @@ class MainController extends AppController
 	public function initialize()
 	{
 		$this->viewBuilder()->setLayout('main');
+		$this->loadModel('Discounts');
+		$this->loadModel('Tickets');
 	}
 
 	public function index()
 	{
+	}
+
+	// 料金割引一覧のアクション
+	public function ticketDiscount(){
+		
+		$discounts = $this->Discounts->find('all');
+		$tickets = $this->Tickets->find('all');
+        $this->set(compact('discounts','tickets'));
 	}
 }
