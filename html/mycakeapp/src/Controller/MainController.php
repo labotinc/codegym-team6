5bf1d546b500cad85fc399b4fd8b4a0d0ef96f59
Satu <kttth0320@gmail.com>
2021-02-25 06:05:41 +0900

    レイアウトテンプレートの無効化

--- html/mycakeapp/src/Controller/MainController.php
+++ html/mycakeapp/src/Controller/MainController.php
@@ -19,7 +19,9 @@ class MainController extends AppController
 
 	// 料金割引一覧のアクション
 	public function ticketDiscount(){
-		
+		// レイアウトテンプレートの無効化
+		$this->layout = false;
+		// DBから情報を取得
 		$discounts = $this->Discounts->find('all');
 		$tickets = $this->Tickets->find('all');
         $this->set(compact('discounts','tickets'));
