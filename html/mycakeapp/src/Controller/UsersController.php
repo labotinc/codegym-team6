d499e7da0c90a37e60d4054d0875944c65a14e22
shihoono <shiho1449@gmail.com>
2021-02-25 11:53:16 +0900

    DBへの保存に失敗した場合のリダイレクト先を追加（現時点ではsignupページに設定）

--- html/mycakeapp/src/Controller/UsersController.php
+++ html/mycakeapp/src/Controller/UsersController.php
@@ -116,6 +116,8 @@ class UsersController extends AppController
 				$session->write('session.signup', $user);
                 return $this->redirect(['action' => 'confirm']);
             }
+			//現時点ではエラー画面が未実装なためsignupページにリダイレクト
+			return $this->redirect(['action' => 'signup']);
             $this->Flash->error(__('保存に失敗しました。もう一度入力下さい。'));
         }
         $this->set(compact('user'));
