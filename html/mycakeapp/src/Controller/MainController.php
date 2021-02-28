dfbf5cff84d0074b8da40209a9d40385f2b817dc
Satu <kttth0320@gmail.com>
2021-03-01 07:45:38 +0900

    Controllerの作成

--- html/mycakeapp/src/Controller/MainController.php
+++ html/mycakeapp/src/Controller/MainController.php
@@ -9,9 +9,18 @@ class MainController extends AppController
 	public function initialize()
 	{
 		$this->viewBuilder()->setLayout('main');
+		// mypageからの遷移先である予約・決済のModel読み込み
+		$this->loadModel('Reservations');
+		$this->loadModel('Payments');
+
 	}
 
 	public function index()
 	{
+	}
+
+	public function mypage()
+	{
+		// mypageから予約決済に遷移する上で必要なデータをモデル経由で取得しviewにわたす処理（遷移先が作成されてから記述）
 	}
 }
