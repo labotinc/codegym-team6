3a19be049258e360a5db29ae5dc1d7765a072637
Satu <kttth0320@gmail.com>
2021-03-01 10:00:47 +0900

    アクションをMainControllerに追加

--- html/mycakeapp/src/Controller/MainController.php
+++ html/mycakeapp/src/Controller/MainController.php
@@ -9,6 +9,9 @@ class MainController extends AppController
 	public function initialize()
 	{
 		$this->viewBuilder()->setLayout('main');
+		// mypageからの遷移先である予約・決済のModel読み込み
+		$this->loadModel('Reservations');
+		$this->loadModel('Payments');
 		$this->loadModel('Discounts');
 		$this->loadModel('Tickets');
 	}
@@ -17,13 +20,19 @@ class MainController extends AppController
 	{
 	}
 
+	public function mypage()
+	{
+		// mypageから予約決済に遷移する上で必要なデータをモデル経由で取得しviewにわたす処理（遷移先が作成されてから記述）
+	}
+
 	// 料金割引一覧のアクション
-	public function ticketDiscount(){
+	public function ticketDiscount()
+	{
 		// レイアウトテンプレートの無効化
 		$this->layout = false;
 		// DBから情報を取得
 		$discounts = $this->Discounts->find('all');
 		$tickets = $this->Tickets->find('all');
-        $this->set(compact('discounts','tickets'));
+		$this->set(compact('discounts', 'tickets'));
 	}
 }
