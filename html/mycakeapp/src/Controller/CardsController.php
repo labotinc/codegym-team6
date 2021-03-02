bc7a4e6b3ed8c9a57af23878cb5a3e1c22285710
hayato-nima <kdc98bac72xkx@gmail.com>
2021-03-02 14:47:58 +0900

    インデントの変更

--- html/mycakeapp/src/Controller/CardsController.php
+++ html/mycakeapp/src/Controller/CardsController.php
@@ -13,185 +13,185 @@ use App\Controller\AppController;
  */
 class CardsController extends AppController
 {
-    /**
-     * Index method
-     *
-     * @return \Cake\Http\Response|null
-     */
-    public function index()
-    {
-        $this->paginate = [
-            'contain' => ['Users'],
-        ];
-        $cards = $this->paginate($this->Cards);
+	/**
+	 * Index method
+	 *
+	 * @return \Cake\Http\Response|null
+	 */
+	public function index()
+	{
+		$this->paginate = [
+			'contain' => ['Users'],
+		];
+		$cards = $this->paginate($this->Cards);
 
-        $this->set(compact('cards'));
-    }
+		$this->set(compact('cards'));
+	}
 
-    /**
-     * View method
-     *
-     * @param string|null $id Card id.
-     * @return \Cake\Http\Response|null
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function view($id = null)
-    {
-        $card = $this->Cards->get($id, [
-            'contain' => ['Users', 'Payments'],
-        ]);
+	/**
+	 * View method
+	 *
+	 * @param string|null $id Card id.
+	 * @return \Cake\Http\Response|null
+	 * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+	 */
+	public function view($id = null)
+	{
+		$card = $this->Cards->get($id, [
+			'contain' => ['Users', 'Payments'],
+		]);
 
-        $this->set('card', $card);
-    }
+		$this->set('card', $card);
+	}
 
-    /**
-     * Add method
-     *
-     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
-     */
-    public function add()
-    {
-        $card = $this->Cards->newEntity();
-        if ($this->request->is('post')) {
-            $card = $this->Cards->patchEntity($card, $this->request->getData());
-            if ($this->Cards->save($card)) {
-                $this->Flash->success(__('The card has been saved.'));
+	/**
+	 * Add method
+	 *
+	 * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
+	 */
+	public function add()
+	{
+		$card = $this->Cards->newEntity();
+		if ($this->request->is('post')) {
+			$card = $this->Cards->patchEntity($card, $this->request->getData());
+			if ($this->Cards->save($card)) {
+				$this->Flash->success(__('The card has been saved.'));
 
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The card could not be saved. Please, try again.'));
-        }
-        $users = $this->Cards->Users->find('list', ['limit' => 200]);
-        $this->set(compact('card', 'users'));
-    }
+				return $this->redirect(['action' => 'index']);
+			}
+			$this->Flash->error(__('The card could not be saved. Please, try again.'));
+		}
+		$users = $this->Cards->Users->find('list', ['limit' => 200]);
+		$this->set(compact('card', 'users'));
+	}
 
-    /**
-     * Edit method
-     *
-     * @param string|null $id Card id.
-     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function edit($id = null)
-    {
-        $card = $this->Cards->get($id, [
-            'contain' => [],
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $card = $this->Cards->patchEntity($card, $this->request->getData());
-            if ($this->Cards->save($card)) {
-                $this->Flash->success(__('The card has been saved.'));
+	/**
+	 * Edit method
+	 *
+	 * @param string|null $id Card id.
+	 * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
+	 * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+	 */
+	public function edit($id = null)
+	{
+		$card = $this->Cards->get($id, [
+			'contain' => [],
+		]);
+		if ($this->request->is(['patch', 'post', 'put'])) {
+			$card = $this->Cards->patchEntity($card, $this->request->getData());
+			if ($this->Cards->save($card)) {
+				$this->Flash->success(__('The card has been saved.'));
 
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The card could not be saved. Please, try again.'));
-        }
-        $users = $this->Cards->Users->find('list', ['limit' => 200]);
-        $this->set(compact('card', 'users'));
-    }
+				return $this->redirect(['action' => 'index']);
+			}
+			$this->Flash->error(__('The card could not be saved. Please, try again.'));
+		}
+		$users = $this->Cards->Users->find('list', ['limit' => 200]);
+		$this->set(compact('card', 'users'));
+	}
 
-    /**
-     * Delete method
-     *
-     * @param string|null $id Card id.
-     * @return \Cake\Http\Response|null Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $card = $this->Cards->get($id);
-        if ($this->Cards->delete($card)) {
-            $this->Flash->success(__('The card has been deleted.'));
-        } else {
-            $this->Flash->error(__('The card could not be deleted. Please, try again.'));
-        }
+	/**
+	 * Delete method
+	 *
+	 * @param string|null $id Card id.
+	 * @return \Cake\Http\Response|null Redirects to index.
+	 * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+	 */
+	public function delete($id = null)
+	{
+		$this->request->allowMethod(['post', 'delete']);
+		$card = $this->Cards->get($id);
+		if ($this->Cards->delete($card)) {
+			$this->Flash->success(__('The card has been deleted.'));
+		} else {
+			$this->Flash->error(__('The card could not be deleted. Please, try again.'));
+		}
 
-        return $this->redirect(['action' => 'index']);
-    }
+		return $this->redirect(['action' => 'index']);
+	}
 
-    public function credit()
-    {
-        $this->loadComponent('Flash');
-        $this->viewBuilder()->setLayout('main');
+	public function credit()
+	{
+		$this->loadComponent('Flash');
+		$this->viewBuilder()->setLayout('main');
 
-        $card = $this->Cards->newEntity();
+		$card = $this->Cards->newEntity();
 
-        if ($this->request->is('post')) {
-            $cardcount = $this->Cards->find()->where(['user_id' => 1])->count(); //user_idは仮の値です
+		if ($this->request->is('post')) {
+			$cardcount = $this->Cards->find()->where(['user_id' => 1])->count(); //user_idは仮の値です
 
-            $card_number = $this->request->getData('card_number');
-            //正規表現でカード番号の整合性がとれているかチェックする
-            $mc = preg_match("/^(5[1-5]\\d{14})|(2(?:22[1-9]|2[3-9][0-9]|[3-6][0-9]{2}|7[0-1][0-9]|720)\\d{12})$/", $card_number);
-            $visa = preg_match("/^4\\d{12}(\\d{3})?$/", $card_number);
+			$card_number = $this->request->getData('card_number');
+			//正規表現でカード番号の整合性がとれているかチェックする
+			$mc = preg_match("/^(5[1-5]\\d{14})|(2(?:22[1-9]|2[3-9][0-9]|[3-6][0-9]{2}|7[0-1][0-9]|720)\\d{12})$/", $card_number);
+			$visa = preg_match("/^4\\d{12}(\\d{3})?$/", $card_number);
 
-            //クレジットカードのバリデーションエラーメッセージ
-            if (!$card_number) { //空白だったら
-                $card->errors('card_number', '空白になっています');
-            } elseif (!is_numeric($card_number)) { //半角数字ではない場合
-                $card->errors('card_number', '半角数字以外の文字が使われています');
-            } elseif (($mc === 0) && ($visa === 0)) {
-                $card->errors('card_number', '不正なカード番号です');
-            } else if ($cardcount >= 2) {
-                $card->errors('card_number', '登録できるカードは１人２枚までです');
-            }
+			//クレジットカードのバリデーションエラーメッセージ
+			if (!$card_number) { //空白だったら
+				$card->errors('card_number', '空白になっています');
+			} elseif (!is_numeric($card_number)) { //半角数字ではない場合
+				$card->errors('card_number', '半角数字以外の文字が使われています');
+			} elseif (($mc === 0) && ($visa === 0)) {
+				$card->errors('card_number', '不正なカード番号です');
+			} else if ($cardcount >= 2) {
+				$card->errors('card_number', '登録できるカードは１人２枚までです');
+			}
 
-            //セキュリティコードのフォームのバリデーションエラーメッセージ
-            $securitycode = $this->request->getData('securitycode');
-            if (!$securitycode) { //空白だったら
-                $card->errors('securitycode', '空白になっています');
-            } elseif (!is_numeric($securitycode)) { //半角数字ではない場合
-                $card->errors('securitycode', '半角数字以外の文字が使われています');
-            }
+			//セキュリティコードのフォームのバリデーションエラーメッセージ
+			$securitycode = $this->request->getData('securitycode');
+			if (!$securitycode) { //空白だったら
+				$card->errors('securitycode', '空白になっています');
+			} elseif (!is_numeric($securitycode)) { //半角数字ではない場合
+				$card->errors('securitycode', '半角数字以外の文字が使われています');
+			}
 
-            //チェックボックスにチェックがない場合エラーメッセージを表示する
-            if (($_POST['terms']['check']) === '0') {
-                $card->errors('terms', '登録には利用規約に同意が必要です');
-            }
+			//チェックボックスにチェックがない場合エラーメッセージを表示する
+			if (($_POST['terms']['check']) === '0') {
+				$card->errors('terms', '登録には利用規約に同意が必要です');
+			}
 
-            //暗号化
-            $plan_text = $card_number; // 暗号化するデータ(カード番号)
-            $method = 'aes-256-ecb'; // 暗号化メソッド
-            $key = 'keykeykey'; // 暗号化キー
-            $ciphertext = openssl_encrypt($plan_text, $method, $key);
+			//暗号化
+			$plan_text = $card_number; // 暗号化するデータ(カード番号)
+			$method = 'aes-256-ecb'; // 暗号化メソッド
+			$key = 'keykeykey'; // 暗号化キー
+			$ciphertext = openssl_encrypt($plan_text, $method, $key);
 
-            //エラーメッセージの上書きを避けるためsaveの条件を満たしている時のみ暗号化したカード番号を$dataに登録
-            if ((($_POST['terms']['check']) === '1') && (is_numeric($securitycode)) && ($mc === 1 || $visa === 1) && ($cardcount <= 1)) {
-                $data = array(
-                    'user_id' => 1, //ログインユーザーの仮の値
-                    'card_number' => $ciphertext,
-                    'expiration_date' => $this->request->getData('expiration_date'),
-                    'name' => $this->request->getData('name'),
-                    'is_deleted' => 0,
-                );
-            } else {
-                $data = array(
-                    'user_id' => 1, //ログインユーザーの仮の値
-                    'expiration_date' => $this->request->getData('expiration_date'),
-                    'name' => $this->request->getData('name'),
-                    'is_deleted' => 0,
-                );
-            }
+			//エラーメッセージの上書きを避けるためsaveの条件を満たしている時のみ暗号化したカード番号を$dataに登録
+			if ((($_POST['terms']['check']) === '1') && (is_numeric($securitycode)) && ($mc === 1 || $visa === 1) && ($cardcount <= 1)) {
+				$data = array(
+					'user_id' => 1, //ログインユーザーの仮の値
+					'card_number' => $ciphertext,
+					'expiration_date' => $this->request->getData('expiration_date'),
+					'name' => $this->request->getData('name'),
+					'is_deleted' => 0,
+				);
+			} else {
+				$data = array(
+					'user_id' => 1, //ログインユーザーの仮の値
+					'expiration_date' => $this->request->getData('expiration_date'),
+					'name' => $this->request->getData('name'),
+					'is_deleted' => 0,
+				);
+			}
 
-            //有効期限は日付の'日'の部分は01を指定する
-            $data['expiration_date'] = array_merge($data['expiration_date'], array('day' => '01'));
-            $card = $this->Cards->patchEntity($card, $data);
+			//有効期限は日付の'日'の部分は01を指定する
+			$data['expiration_date'] = array_merge($data['expiration_date'], array('day' => '01'));
+			$card = $this->Cards->patchEntity($card, $data);
 
-            //利用規約にチェックしていて、セキュリティーコードを数字で入力していて、カード番号の整合性がとれていて、カードの登録数が１枚以下の場合にsaveを実行する
-            if ((($_POST['terms']['check']) === '1') && (is_numeric($securitycode)) && ($mc === 1 || $visa === 1) && ($cardcount <= 1)) {
-                if ($this->Cards->save($card)) {
-                    return $this->redirect(['action' => 'confirm']);
-                }
-            }
+			//利用規約にチェックしていて、セキュリティーコードを数字で入力していて、カード番号の整合性がとれていて、カードの登録数が１枚以下の場合にsaveを実行する
+			if ((($_POST['terms']['check']) === '1') && (is_numeric($securitycode)) && ($mc === 1 || $visa === 1) && ($cardcount <= 1)) {
+				if ($this->Cards->save($card)) {
+					return $this->redirect(['action' => 'confirm']);
+				}
+			}
 
-            $this->Flash->error(__('保存に失敗しました。もう一度入力下さい。'));
-        }
+			$this->Flash->error(__('保存に失敗しました。もう一度入力下さい。'));
+		}
 
-        $this->set(compact('card'));
-    }
+		$this->set(compact('card'));
+	}
 
-    public function confirm()
-    {
-        $this->viewBuilder()->setLayout('main');
-    }
+	public function confirm()
+	{
+		$this->viewBuilder()->setLayout('main');
+	}
 }
